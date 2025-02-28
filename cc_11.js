ddf043c386664f4fe79511eff5c022353286e5d3
Kamali Hicks <kamalikh3@gmail.com>
2025-02-28 17:45:54 -0500

    "Task 2 - Created Borrower Class"

--- cc_11.js
+++ cc_11.js
@@ -1,3 +1,4 @@
+// Task 1 Creating a book class
 class Book { // Book Class
     constructor(title, author, isbn, copies) { // Class properties
         this.title = title;
@@ -10,10 +11,42 @@ class Book { // Book Class
     };
     updateCopies(quantity) { // Method to update stock quantities
         this.copies += quantity
-    }
+    };
 }
 
 const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5); // Test
 console.log(book1.getDetails()); // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 5"
 book1.updateCopies(-1); // Test
 console.log(book1.getDetails()); // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+
+// Task 2 Creating a borrower class
+
+class Borrower {
+    constructor(name, borrowerID) {
+        this.name = name;
+        this.borrowerID = borrowerID;
+        this.borrowedBooks = [];
+    };
+    borrowBook(book) { // Method to borrow books
+        if (!this.borrowedBooks.includes(book)) { // Check to see if books is already borrowed
+            this.borrowedBooks.push(book); // If not borrowed add to the borrowed books array
+        } else {
+            console.log(`${book} is already borrowed. `);
+        };
+    };
+    returnBook(book) { // Method to return books
+       const index = this.borrowedBooks.indexOf(book); 
+        if (index !== -1) { // Find book in the borrowed books array
+            this.borrowedBooks.splice(index, 1) // Remove book from borrowed books array 
+        } else {
+            console.log(`${book} hasn't been borrowed`) // Message if book was not found in array
+        }
+    }
+}
+// Test 
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks); // Expected output: ["The Great Gatsby"]
+
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks); // Expected output: []
